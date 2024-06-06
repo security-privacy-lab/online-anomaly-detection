1bd5ea7b7ead14a08bd874878300c25df6f7782e
Rudy Ramos <18382017+rramos-code@users.noreply.github.com>
2024-06-06 18:19:29 +0000

    Formatting

--- Graph Modeling/graph-creation.py
+++ Graph Modeling/graph-creation.py
@@ -14,7 +14,7 @@ df["timestamp_minute"] = df["Timestamp"].dt.floor("min")
 # we sort the dataframe by timestamp earliest to latest...
 df = df.sort_values(by=["Timestamp"])
 # and group the entries by minute...
-grouped = df.groupby('timestamp_minute')
+grouped = df.groupby("timestamp_minute")
 # We then grab the group length for progress tracking, but this is an optional step that can be removed.
 total_groups = len(grouped)
 progress_counter = 0
@@ -33,72 +33,74 @@ for timestamp_minute, group in grouped:
     # Now, for each entry...
     for idx, row in group.iterrows():
         # Record the source and destination IP, as well as the flow ID.
-        src_ip = row['Src IP']
-        dst_ip = row['Dst IP']
-        flow_id = row['Flow ID'] # Currently, this isn't being used, but again, you may want to use this for indexing?
-        
+        src_ip = row["Src IP"]
+        dst_ip = row["Dst IP"]
+        flow_id = row[
+            "Flow ID"
+        ]  # Currently, this isn't being used, but again, you may want to use this for indexing?
+
         # Initialize node features dictionary with common attributes for both forward and backward nodes
         node_features = {
-            'src_ip': src_ip,
-            'dst_ip': dst_ip,
-            'flow_id': flow_id,
-            'direction': 'bidirectional'  # Adding a direction attribute to distinguish between forward and backward nodes
+            "src_ip": src_ip,
+            "dst_ip": dst_ip,
+            "flow_id": flow_id,
+            "direction": "bidirectional",  # Adding a direction attribute to distinguish between forward and backward nodes
         }
-        
+
         # Update the dictionary with specific attributes for the forward node
         # Another thing to note is that for pytorch-geo use, you need the features to share the same attributes, so the names are now shared, although the values
         # are being pulled from different places for forwards and backwards nodes.
         forward_features = {
-            'total_pkts': row['Total Fwd Packet'],
-            'pkts_per_s': row['Fwd Packets/s'],
-            'iat_std': row['Fwd IAT Std'],
-            'total_len_pkt': row['Total Length of Fwd Packet'],
-            'pkt_len_std': row['Fwd Packet Length Std'],
-            'seg_size_avg': row['Fwd Segment Size Avg'],
-            'init_win_bytes': row['FWD Init Win Bytes'],
-            'pkt_len_mean': row['Fwd Packet Length Mean'],
-            'iat_max': row['Fwd IAT Max'],
-            'avg_pkt_size': row['Average Packet Size'],
-            'subflow_bytes': row['Subflow Fwd Bytes'],
-            'ip': src_ip,
-            'direction': 'forward'
+            "total_pkts": row["Total Fwd Packet"],
+            "pkts_per_s": row["Fwd Packets/s"],
+            "iat_std": row["Fwd IAT Std"],
+            "total_len_pkt": row["Total Length of Fwd Packet"],
+            "pkt_len_std": row["Fwd Packet Length Std"],
+            "seg_size_avg": row["Fwd Segment Size Avg"],
+            "init_win_bytes": row["FWD Init Win Bytes"],
+            "pkt_len_mean": row["Fwd Packet Length Mean"],
+            "iat_max": row["Fwd IAT Max"],
+            "avg_pkt_size": row["Average Packet Size"],
+            "subflow_bytes": row["Subflow Fwd Bytes"],
+            "ip": src_ip,
+            "direction": "forward",
         }
         node_features.update(forward_features)
-        
+
         # Add the forward node to the graph
         forward_node_id = f"{index}_fwd"
         G.add_node(forward_node_id, **node_features)
         nodes_list.append(forward_node_id)
-        
+
         # Update the dictionary with specific attributes for the backward node
         backward_features = {
-            'total_pkts': row['Total Bwd packets'],
-            'pkts_per_s': row['Bwd Packets/s'],
-            'iat_std': row['Bwd IAT Std'],
-            'total_len_pkt': row['Total Length of Bwd Packet'],
-            'pkt_len_std': row['Bwd Packet Length Std'],
-            'seg_size_avg': row['Bwd Segment Size Avg'],
-            'init_win_bytes': row['Bwd Init Win Bytes'],
-            'pkt_len_mean': row['Bwd Packet Length Mean'],
-            'iat_max': row['Bwd IAT Max'],
-            'avg_pkt_size': row['Average Packet Size'],
-            'subflow_bytes': row['Subflow Bwd Bytes'],
-            'ip': dst_ip,
-            'direction': 'backward'
+            "total_pkts": row["Total Bwd packets"],
+            "pkts_per_s": row["Bwd Packets/s"],
+            "iat_std": row["Bwd IAT Std"],
+            "total_len_pkt": row["Total Length of Bwd Packet"],
+            "pkt_len_std": row["Bwd Packet Length Std"],
+            "seg_size_avg": row["Bwd Segment Size Avg"],
+            "init_win_bytes": row["Bwd Init Win Bytes"],
+            "pkt_len_mean": row["Bwd Packet Length Mean"],
+            "iat_max": row["Bwd IAT Max"],
+            "avg_pkt_size": row["Average Packet Size"],
+            "subflow_bytes": row["Subflow Bwd Bytes"],
+            "ip": dst_ip,
+            "direction": "backward",
         }
         node_features.update(backward_features)
-        
+
         # Add the backward node to the graph
         backward_node_id = f"{index}_bkd"
         G.add_node(backward_node_id, **node_features)
         nodes_list.append(backward_node_id)
-        
+
         # Connect the edges as appropriate (in this case, chronological order, as we are dealing with network traffic.)
         G.add_edge(forward_node_id, backward_node_id)
         # we then connect the previous backwards node to the next forward node, although we may need to change this in the future.
         if previous_backward_node is not None:
             G.add_edge(previous_backward_node, forward_node_id)
-        
+
         previous_backward_node = backward_node_id
         index += 1
 
@@ -113,6 +115,6 @@ for timestamp_minute, group in grouped:
 
     # Print progress as percentage
     progress_percentage = (progress_counter / total_groups) * 100
-    print(f"Progress: {progress_percentage:.2f}% complete", end='\r')
+    print(f"Progress: {progress_percentage:.2f}% complete", end="\r")
 
 print("Processing complete.")
