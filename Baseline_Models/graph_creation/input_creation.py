6c208c6d31cb18b88b01a81ece0776daf493d757
Yongyi Zhou <yz220@rice.edu>
2024-06-30 21:50:34 -0500

    refactor graph_selection

--- Baseline_Models/graph_creation/input_creation.py
+++ Baseline_Models/graph_creation/input_creation.py
@@ -1,11 +1,14 @@
 import json
 import argparse
 import networkx as nx
+import os
 import random
 from networkx.readwrite import json_graph
 from pathlib import Path
+from collections import defaultdict
 
 MODELS = ["gin", "graphsage"]
+K = 10
 
 
 def process_data(models):
@@ -56,6 +59,7 @@ def graphsage():
 def gin():
     print("Processing data for GIN...")
 
+    # Locating the pygraphs.json file and loading it into an NX Graph object
     path = Path(__file__).parent / "processed_data" / "pygraphs.json"
 
     with open(path, "r") as f:
@@ -65,12 +69,59 @@ def gin():
     for graph in data["graphs"]:
         nx_graphs.append(json_graph.node_link_graph(graph))
 
-    print(len(nx_graphs))
-    random.shuffle(nx_graphs)
+    num_graphs = len(nx_graphs)
+
+    # Looping through all the graphs and assigning them to folds and
+    # coverting them into appropriate format
+
+    file = f"{num_graphs}\n"
+
     for graph in nx_graphs:
+
+        features = nx.get_node_attributes(graph, "features")
+
+        num_nodes = len(graph.nodes)
+
+        labels = nx.get_node_attributes(graph, "label")
+
+        graph_label = 1 if any(
+            node_label == 1 for node_label in labels.values()) else 0
+
+        # Adding the graph metadata
+        file += f"{num_nodes} {graph_label}\n"
+
+        # Now adding the information of each node
         for node in graph.nodes:
-            in_edges = graph.in_edges(node)
-            out_edges = graph.out_edges(node)
+            neighbors = list(nx.all_neighbors(graph, node))
+            node_features = features[node]
+            file += f"{node} {len(neighbors)} {' '.join([str(n) for n in neighbors])} {' '.join([str(f) for f in node_features])}\n"
+
+    directory = Path(__file__).parent / "processed_data" / "gin_data"
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # final_data = dict()
+    # for fold_primary in range(K):
+    #     str_content = ""
+    #     num_nodes = 0
+    #     for fold_secondary in range(K):
+    #         if fold_primary != fold_secondary:
+    #             str_content += folds[fold_secondary][0]
+    #             num_nodes += folds[fold_secondary][1]
+    #     str_content = str(num_nodes) + "\n" + str_content
+    #     final_data[fold_primary] = {
+    #         "train": str_content, "test": folds[fold_primary][0]}
+
+    with open(directory/"data.txt", 'w+') as f:
+        f.write(file)
+
+    gin_directory = Path(__file__).parent.parent/"GIN"/"dataset"/"DARKNET"
+    if not os.path.exists(gin_directory):
+        os.makedirs(gin_directory)
+
+    with open(gin_directory/"DARKNET.txt", 'w+') as f:
+        f.write(file)
 
     print("Data for GIN successfully processed")
 
