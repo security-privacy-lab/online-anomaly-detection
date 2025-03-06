2e30e11b966029c3537d589d1add0335b1409213
Yongyi Zhou <yz220@rice.edu>
2025-03-05 19:39:45 -0600

    debug and add comment; 0.50 AUC

--- WUIL/simple_model.py
+++ WUIL/simple_model.py
@@ -4,6 +4,14 @@ from sklearn.metrics import roc_auc_score
 
 class SimpleModel:
 
+    # Notes for change:
+    # some damping factor for the avg? Similar to that of MIDAS
+    # benign users often revisit the same folders (you ever only use some specific folders as a normal user)
+    #   extra feature: weight of the edge - if weight is low then anomaly score should be high
+    #   either sliding window on edges or damping factor
+    # burst of edges?
+    # use CMS to keep track of weight
+    
     def __init__(self, threshold):
         self._threshold = threshold
         
@@ -17,12 +25,22 @@ class SimpleModel:
     Function to determine if data is malicious (True) or not (False)
     """
     def classify(self, depth_delta, time_delta):
-        depth_z_score = ((depth_delta - self._depth_avg)**2)/math.sqrt(self._depth_var)
-        time_z_score = ((time_delta - self._time_avg)**2)/math.sqrt(self._time_var)
 
+        # To avoid division by zero errors
+        if self._depth_var == 0:
+            self._depth_var = 0.001
+        if self._time_var == 0:
+            self._time_var = 0.001
+
+        depth_z_score = ((depth_delta - self._depth_avg))/math.sqrt(self._depth_var)
+        time_z_score = ((time_delta - self._time_avg))/math.sqrt(self._time_var)
+
+        # Find the sum of deviations from mean (z-score)
         sum_z = (abs(depth_z_score) + abs(time_z_score))
+        sum_z = abs(depth_z_score)
 
-        if sum_z > self._threshold:
+        # Predict
+        if sum_z/2 > self._threshold:
             return True
         else:
             return False
@@ -31,25 +49,33 @@ class SimpleModel:
     Function to update the average and variance of depth delta and time delta
     """
     def update_model(self, depth_delta, time_delta, size):
+
+        # Calculate the new averages
         new_depth_avg = self._depth_avg + (depth_delta-self._depth_avg)/(size+1)
         new_time_avg = self._time_avg + (time_delta-self._time_avg)/(size+1)
         
+        # Calculate the new variances
         new_depth_var = ((size-1) * self._depth_var + (depth_delta-self._depth_var) * (depth_delta-new_depth_avg))/(size)
         new_time_var = ((size-1) * self._time_var + (time_delta-self._time_var) * (time_delta-new_time_avg))/(size)
 
-
+        # Update the metrics
         self._depth_avg = new_depth_avg
         self._time_avg = new_time_avg
 
         self._depth_var = new_depth_var
         self._time_var = new_time_var
 
+    """
+    Function to calculate the longest common path parent directory
+    """
     def commonpath(self, path1: Path, path2: Path) -> Path:
         i = 0
         common_path = Path("")
+
+        # Iterate until the parent directory does not match
         while i < len(path1.parts) and i < len(path2.parts):
             if path1.parts[i] == path2.parts[i]:
-                common_path = common_path.joinpath(path1.parts[i])
+                common_path = common_path / path1.parts[i]
             else:
                 break
             i += 1
@@ -62,46 +88,63 @@ class SimpleModel:
     def run_model(self, filename):
         predictions = []
         labels = []
+
         with open(filename, 'r') as f:
             size = 0
             prev_time = 0
+
+            # Iterate through every line in the data file
             for line in f:
                 size += 1
                 data = line.split(",")
                 if len(data) != 4:
-                    print("Invalid data fof simple_model. Must be in the form [timestamp, src_node, dst_node, label]")
+                    print("Invalid data of simple_model. Must be in the form [timestamp, src_node, dst_node, label]")
                     return
-                # Unpack the group data
-                timestamp = data[0]
+                
+                # Unpack the data
+                timestamp = int(data[0])
                 src_node = Path(data[1])
                 dst_node = Path(data[2])
                 
                 label = data[3]
                 labels.append(int(label))
                 
+                # Make sure prev_time is instantiated
                 if size == 1:
-                    prev_time = int(timestamp)
+                    prev_time = timestamp
                 
-                depth_delta = len(Path(dst_node).parts) - len(self.commonpath(src_node, dst_node).parts)
-                time_delta = int(timestamp) - prev_time
-                if size < 100:
-                    self.update_model(depth_delta, time_delta, size)
+                # Ignore data when user is idle
+                if dst_node == src_node:
                     predictions.append(0)
+                    size -= 1
                 else:
-                    is_mal = self.classify(depth_delta, time_delta)
-                    predictions.append(int(is_mal))
+
+                    # Find time difference and path distance
+                    depth_delta = len(Path(dst_node).parts) + len(Path(src_node).parts) - 2*len(self.commonpath(src_node, dst_node).parts)
+                    time_delta = int(timestamp) - prev_time
                     
-                    if not is_mal:
+                    # Instantiate the average and variance metrics with the first two data points (idle data is ignored)
+                    if size < 3:
                         self.update_model(depth_delta, time_delta, size)
+                        predictions.append(0)
+                    else:
+                        is_mal = self.classify(depth_delta, time_delta)
+                        predictions.append(int(is_mal))
+                        
+                        # Update the model only if the data is not malicious
+                        if not is_mal:
+                            self.update_model(depth_delta, time_delta, size)
         
-        print(roc_auc_score(labels, predictions))
+                prev_time = timestamp
+        print(roc_auc_score(labels, predictions), self._depth_avg, self._time_avg)
 
 
 
 
 # Sanity check
 model = SimpleModel(2)
-model.run_model("./custom_data/simple_mode_data.csv")
+model.run_model("./custom_data/simple_model_data.csv")
+model.run_model("./custom_data/simple_model_benign.csv")
 
 
                     
