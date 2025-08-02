8183fdfaf05c5a6b96f262ceab0ef9ee494616b7
Jseo09 <seojin929@gmail.com>
2025-08-02 15:25:40 -0500

    added f-fade files

---
+++ Baseline_Models/F-FADE-master/run.sh
@@ -0,0 +1,1 @@
+python main.py --dataset ./data/user12_anomrank_combined_5mc.txt --embedding_size 200 --t_setup 8000 --W_upd 720 --T_th 120 --alpha 0.999 --epoch 5 --online_train_steps 10 --M 100 --model_dir ./result/
